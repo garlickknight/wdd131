4be7296c70c27f1555b705879a22c9135fb0ddbc
garlickknight <garlickknight@gmail.com>
2025-09-27 20:49:55 -0600

    commit

--- scripts/place.js
+++ scripts/place.js
@@ -1,7 +1,6 @@
-var date = new Date();
-var year = date.getFullYear();
-document.getElementById("currentyear").innerHTML = year;
-document.getElementById("lastModified").innerHTML = document.lastModified;
-
-let lastModified = date.parse(document.lastModified);
-document.getElementById("lastmodified").innerHTML = lastModified
+// var date = new Date();
+// var year = date.getFullYear();
+// document.getElementById("currentyear").innerHTML = year;
+// document.getElementById("lastModified").innerHTML = document.lastModified;
+// let lastModified = date.parse(document.lastModified);
+// document.getElementById("lastmodified").innerHTML = lastModified
