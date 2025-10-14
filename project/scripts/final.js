889c2871bc43ed3736b08bb58c849070ad6c387a
garlickknight <garlickknight@gmail.com>
2025-10-14 16:57:53 -0600

    test

---
+++ project/scripts/final.js
@@ -0,0 +1,5 @@
+var date = new Date();
+var year = date.getFullYear();
+document.getElementById("currentyear").innerHTML = year;
+// let modified = alert(document.lastModified);
+document.getElementById("lastModified").innerHTML = document.lastModified;
